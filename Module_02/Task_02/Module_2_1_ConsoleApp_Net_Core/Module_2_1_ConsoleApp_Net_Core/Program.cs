8db3b503ab8f8b3f2d421f398aa4f544240fe03d
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-09-21 22:59:39 +0200

    added ConsoleApp

---
+++ Module_02/Task_02/Module_2_1_ConsoleApp_Net_Core/Module_2_1_ConsoleApp_Net_Core/Program.cs
@@ -0,0 +1,16 @@
+﻿using HelloLibrary;
+
+namespace Module_2_1_ConsoleApp_Net_Core
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var parameter in args)
+            {
+                var output = StringLibrary.ConcatenateString(parameter);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
