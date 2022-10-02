6011995584d119e0053e3cb689ed822dce75e30c
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-10-02 15:48:01 +0200

    start development

--- Module_04/Task_01/FileSystemVisitor/Program.cs
+++ Module_04/Task_01/FileSystemVisitor/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace FileSystemVisitor
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
