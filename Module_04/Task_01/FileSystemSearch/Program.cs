1461d58824afe084b1bbd6d215177ea0752518b5
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-10-06 23:13:25 +0200

    Started/Completed events

--- Module_04/Task_01/FileSystemSearch/Program.cs
+++ Module_04/Task_01/FileSystemSearch/Program.cs
@@ -2,6 +2,13 @@
 {
     public static class Program
     {
+
+        public delegate void SearchStarted();
+        public static event SearchStarted StartedNotify;
+
+        delegate void SearchCompleted();
+        private static event SearchCompleted CompletedNotify;
+
         public static void PrintToConsole(IEnumerable<SearchedItem> array)
         {
             Console.WriteLine("Search result:");
@@ -11,15 +18,31 @@
             }
         }
 
+
         public static void Main(string[] args)
         {
             Console.WriteLine();
             var filter = FilterSetting.GetFilter();
 
+            StartedNotify += FileSystemVisitor_StartedNotify;
+            StartedNotify?.Invoke();
+
             FileSystemVisitor systemVisitor = new(filter);
             var array = systemVisitor.Search(@"C:\FileSystem");
             PrintToConsole(array);
 
+            CompletedNotify += Program_CompletedNotify;
+            CompletedNotify.Invoke();
+        }
+
+        private static void Program_CompletedNotify()
+        {
+            Console.WriteLine("Search finished");
+        }
+
+        private static void FileSystemVisitor_StartedNotify()
+        {
+            Console.WriteLine("Search started!");
         }
     }
 }
