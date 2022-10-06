503d6ebeaf70092fe9439954497343d1fc957884
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-10-06 22:05:15 +0200

    changed date filter

--- Module_04/Task_01/FileSystemSearch/FilterSetting.cs
+++ Module_04/Task_01/FileSystemSearch/FilterSetting.cs
@@ -1,8 +1,11 @@
-﻿namespace FileSystemSearch
+﻿using Console = System.Console;
+
+namespace FileSystemSearch
 {
     public static class FilterSetting
     {
         private static string FileType { get; set; }
+        private static DateTime CreationDate { get; set; }
         private static int NameLength { get; set; }
         private static int GetUserInput()
         {
@@ -12,13 +15,25 @@
                               "3 - File type \n" +
                               "4 - Creation date \n" +
                               "5 - Size");
-
             var input = Convert.ToInt32(Console.ReadLine());
 
             if (input == 3)
             {
                 Console.WriteLine("Enter file type like: .txt");
                 FileType = Console.ReadLine();
+            }
+
+            if (input == 4)
+            {
+                Console.WriteLine("Enter creation year:");
+                var year = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter creation month:");
+                var month = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter creation day:");
+                var day = Convert.ToInt32(Console.ReadLine());
+                CreationDate = new(year, month, day);
             }
 
             if (input == 5)
@@ -40,7 +55,10 @@
                 case 1: return x => !x.IsFolder;
                 case 2: return x => x.IsFolder;
                 case 3: return x => x.FileType == FileType;
-                case 4: return x => x.Date < DateTime.Now;
+                case 4:
+                    return x => x.Date.Year == CreationDate.Year
+                                            && x.Date.Month == CreationDate.Month
+                                            && x.Date.Day == CreationDate.Day;
                 case 5: return x => x.NameLength < NameLength;
                 default: return x => x.IsFolder || !x.IsFolder;
             }
