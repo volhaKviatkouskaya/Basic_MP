2a8d534ce67ee5b451391b0ac790cd4f9a5c988e
Volha <77961930+volhaKuraleniaE@users.noreply.github.com>
2022-10-11 22:13:09 +0200

    File system visitor (#4) (#5)
    * start development
    
    * print folders to Console with recursion
    
    * refactoring
    
    * refactoring
    
    * 1
    
    * refactoring
    
    * remove List variable from instance
    
    * added overloaded constructor with filter
    
    * search method and SearchedItem added
    
    * implement IEnumerable iterator
    
    * IEnumerable iterator returns SearchedItem instance
    
    * added predicate for filterring and overloaded dconstructor
    
    * added filtering
    
    * changed date filter
    
    * Started/Completed events
    
    * .gitignore is now working
    
    * git ignore
    
    * move start/finish events to FileSystemVisitor class and refactor Search method
    
    * notifications added
    
    * EVEnts refactoring
    
    * exclude custom filter
    
    * refactoring
    
    * refactoring
    
    * gitignore
    
    Co-authored-by: Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
    
    Co-authored-by: Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>

---
+++ Module_04/Task_01/FileSystemSearch/FileSystemVisitor.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+
+namespace FileSystemSearch
+{
+    public enum ActionType
+    {
+        ExcludeItem = 1,
+        KeepItem = 2,
+        AbortSearch = 3
+    }
+
+    public class FileSystemVisitor
+    {
+        public event Action StartEvent;
+        public event Action FinishEvent;
+
+        public event Action<SearchedItem> FoundItem;
+
+        public event Func<SearchedItem, ActionType> FoundFilteredItem;
+
+        public Predicate<SearchedItem> Predicate;
+
+        public FileSystemVisitor() { }
+
+        public FileSystemVisitor(Predicate<SearchedItem> predicate) => Predicate = predicate;
+
+        public List<SearchedItem> Search(string path)
+        {
+            StartEvent();
+            var result = SearchRecursive(path).ToList();
+            FinishEvent();
+            return result;
+        }
+
+        public IEnumerable<SearchedItem> SearchRecursive(string path)
+        {
+            foreach (var item in Directory.GetFileSystemEntries(path))
+            {
+                var foundItem = CreateItem(item);
+                FoundItem(foundItem);
+
+                if (Predicate(foundItem))
+                {
+                    var res = FoundFilteredItem(foundItem);
+
+                    if (res == ActionType.ExcludeItem) continue;
+                    if (res == ActionType.KeepItem) yield return foundItem;
+                    if (res == ActionType.AbortSearch) yield break;
+                }
+
+                if (foundItem.IsFolder)
+                {
+                    foreach (var foundInDir in SearchRecursive(foundItem.Name))
+                    {
+                        if (Predicate(foundInDir)) yield return foundInDir;
+                    }
+                }
+            }
+        }
+
+        private SearchedItem CreateItem(string item)
+        {
+            var isDirExist = Directory.Exists(item);
+
+            if (isDirExist)
+            {
+                return new SearchedItem()
+                {
+                    Name = item,
+                    IsFolder = isDirExist,
+                    NameLength = Path.GetDirectoryName(item).Length,
+                    Date = Directory.GetCreationTime(item)
+                };
+            }
+            else
+            {
+                return new SearchedItem()
+                {
+                    Name = item,
+                    IsFolder = isDirExist,
+                    NameLength = Path.GetFileName(item).Length,
+                    FileType = new FileInfo(item).Extension,
+                    Date = File.GetCreationTime(item)
+                };
+            }
+        }
+    }
+}
---
+++ Module_04/Task_01/FileSystemSearch/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace FileSystemSearch
+{
+    public static class Program
+    {
+        public static void PrintToConsole(IEnumerable<SearchedItem> array)
+        {
+            Console.WriteLine("Search result:");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var filter = FilterSetting.GetFilter();
+
+            FileSystemVisitor systemVisitor = new(filter);
+
+            systemVisitor.StartEvent += () => Console.WriteLine("Search started!");
+            systemVisitor.FinishEvent += () => Console.WriteLine("Search finished!");
+
+            systemVisitor.FoundItem += (x) =>
+            {
+                var itemType = x.IsFolder ? "Directory" : "File";
+                Console.WriteLine($"{itemType}  found: {x.Name}");
+            };
+
+            systemVisitor.FoundFilteredItem += (x) =>
+            {
+                var itemType = x.IsFolder ? "directory" : "file";
+                Console.WriteLine($"Filtered {itemType} found: {x.Name}");
+                return ActionType.KeepItem;
+            };
+
+            var array = systemVisitor.Search(@"C:\FileSystem");
+            PrintToConsole(array);
+        }
+    }
+}
