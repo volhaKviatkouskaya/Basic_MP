a51e985ed0db3d842f485db5975b0f954007468c
Volha <77961930+volhaKuraleniaE@users.noreply.github.com>
2022-10-20 22:15:11 +0200

    Module 05 (#8)
    * task for module_05
    
    * task 1
    
    * task 2
    
    * task_02 convert wthout build-in method
    
    * implement check for OverFlowException
    
    * conflict solved
    
    Co-authored-by: Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>

---
+++ Module_05/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            try
+            {
+                Console.WriteLine(input[0]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Attempted to print empty line");
+            }
+        }
+    }
+}
---
+++ Module_05/Task2/NumberParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        private const int Diff = 48;
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null) throw new ArgumentNullException();
+
+            var length = stringValue.Length;
+
+            if (length == 0) throw new FormatException();
+
+            int result = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if ((stringValue[i] == '-' || stringValue[i] == '+') && i == 0)
+                    continue;
+
+                if (i != 0 && char.IsWhiteSpace(stringValue[i]))
+                    continue;
+
+                if (char.IsNumber(stringValue[i]))
+                {
+                    checked
+                    {
+                        if (stringValue[0] == '-')
+                        {
+                            result *= 10;
+                            result -= ((int)stringValue[i] - Diff);
+                        }
+                        else
+                        {
+                            result *= 10;
+                            result += ((int)stringValue[i] - Diff);
+                        }
+                    }
+                }
+                else
+                    throw new FormatException();
+            }
+
+            return result;
+        }
+    }
+}
