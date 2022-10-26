82e052382006f71059f33b9a97386824897bd02f
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-10-26 09:44:39 +0200

    implemented CommandLine in FillterSettings

--- Module_04/Task_01/FileSystemSearch/FilterSetting.cs
+++ Module_04/Task_01/FileSystemSearch/FilterSetting.cs
@@ -1,88 +1,58 @@
 ﻿using System.CommandLine;
-using System.CommandLine.Invocation;
-using Console = System.Console;
 
 namespace FileSystemSearch
 {
-    public static class FilterSetting
+    public class FilterSetting
     {
-        // private static List<string> CommandList = new() { "-help", "-list", "-file", "-folder", "-date",
-        //       "-nameLength", "-fileType" };
+        private Action<string, Predicate<SearchedItem>> filtersPredicate;
+        public FilterSetting(Action<string, Predicate<SearchedItem>> predicate) => filtersPredicate = predicate;
 
-        private static string FileType { get; set; }
-        private static DateTime CreationDate { get; set; }
-        private static int NameLength { get; set; }
-
-        public static void PassConsoleInput(params string[] args)
+        public void PassConsoleInput(params string[] args)
         {
-            var allOption = new Option<bool>(name: "--all", "Search all");
-            var fileOption = new Option<bool>(name: "--file", "Search files only");
-            var foldOption = new Option<bool>(name: "--folder", "Search folders only");
-            var fileTypeOption = new Option<bool>(name: "--filetype", "Search file with type .type");
-            var nameLengthOption = new Option<bool>(name: "--name", "Search file/folder with length name XX");
+            var filterOption = new Option<string>(name: "--filter", "Filter: filter, filterValue");
+            var filterValueOption = new Option<string>(name: "--filter-value", "Filter value: filterValue type, date:DD.MM.YYYY");
+
+            var pathArgument = new Argument<string>(
+                name: "path",
+                description: "Path for search");
 
             var rootCommand = new RootCommand("CommandLine app for search");
 
-            rootCommand.AddOption(allOption);
-            rootCommand.AddOption(fileOption);
-            rootCommand.AddOption(foldOption);
-            rootCommand.AddOption(fileTypeOption);
-            rootCommand.AddOption(nameLengthOption);
+            rootCommand.AddOption(filterOption);
+            rootCommand.AddOption(filterValueOption);
+            rootCommand.AddArgument(pathArgument);
+            rootCommand.SetHandler(GetAdaptation, pathArgument, filterOption, filterValueOption);
 
-            rootCommand.SetHandler((x) =>
-            {
-                Handle(x);
-            });
+
             rootCommand.Invoke(args);
         }
 
-        public static void Handle(InvocationContext cont)
+        public void GetAdaptation(string path, string filter, string filterValue)
         {
-            var options = cont.BindingContext.ParseResult.Tokens.FirstOrDefault().ToString();
-            var filter = FilterSetting.GetFilter(options);
-            FileSystemVisitor systemVisitor = new(filter);
-
-            systemVisitor.StartEvent += () => Console.WriteLine("Search started!");
-            systemVisitor.FinishEvent += () => Console.WriteLine("Search finished!");
-
-            systemVisitor.FoundItem += (x) =>
-            {
-                var itemType = x.IsFolder ? "Directory" : "File";
-                Console.WriteLine($"{itemType}  found: {x.Name}");
-            };
-
-            systemVisitor.FoundFilteredItem += (x) =>
-            {
-                var itemType = x.IsFolder ? "directory" : "file";
-                Console.WriteLine($"Filtered {itemType} found: {x.Name}");
-                Console.WriteLine("Exclude item(1), Keep item(2), Abort search(3)?");
-                var userInput = Convert.ToInt32(Console.ReadLine());
-
-                return (ActionType)userInput;
-            };
-
-            var array = systemVisitor.Search(@"C:\FileSystem");
-
-            Program.PrintToConsole(array);
+            filtersPredicate(path, GetFilter(filter, filterValue));
         }
 
-        public static Predicate<SearchedItem> GetFilter(string arg)
-
+        public Predicate<SearchedItem> GetFilter(string filter, string filterValue)
         {
-            var userInput = arg.ToUpper();
+            var userInput = filter?.ToUpper();
 
             switch (userInput)
             {
-                case "--FILE": return x => !x.IsFolder;
-                case "--FOLDER": return x => x.IsFolder;
-                case "--DATE":
-                    return x => x.Date.Year == CreationDate.Year
-                                && x.Date.Month == CreationDate.Month
-                                && x.Date.Day == CreationDate.Day;
-                case "--NAMELENGTH": return x => x.NameLength == NameLength;
-                case "--FILETYPE": return x => x.FileType == FileType;
+                case "FILE": return x => !x.IsFolder;
+                case "FOLDER": return x => x.IsFolder;
+                case "DATE":
+                    return x => matchesDate(x.Date.Date, filterValue);
+                case "LENGTH": return x => x.NameLength == Convert.ToInt32(filterValue);
+                case "FILETYPE":
+                    return x => x.FileType == filterValue.ToUpper();
                 default: return x => x.IsFolder || !x.IsFolder;
             }
+        }
+
+        private bool matchesDate(DateTime x, string userInput)
+        {
+            var date = DateTime.ParseExact(userInput, "dd.MM.yyyy", null);
+            return x == date;
         }
     }
 }
--- Module_04/Task_01/FileSystemSearch/Program.cs
+++ Module_04/Task_01/FileSystemSearch/Program.cs
@@ -1,64 +1,51 @@
-﻿using System;
-using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Invocation;
-using System.CommandLine.Parsing;
-
-namespace FileSystemSearch
+﻿namespace FileSystemSearch
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            FilterSetting.PassConsoleInput(args);
-            /*
-            var allOption = new Option<bool>(name: "--all", "Select all");
-            var foldOption = new Option<bool>(name: "--folder", "Exclude folders from search");
-
-            var rootCommand = new RootCommand("CommandLine app for search");
-            rootCommand.AddOption(allOption);
-            rootCommand.AddOption(foldOption);
-
-            rootCommand.SetHandler((x) =>
-            {
-                Handle(x);
-            });
-            rootCommand.Invoke(args);
-
-            /*
-            var infinitySearch = true;
+            var infinity = true;
             do
             {
-                var filter = FilterSetting.GetFilter();
-                FileSystemVisitor systemVisitor = new(filter);
+                var args = Console.ReadLine().Split(' ');
 
-                systemVisitor.StartEvent += () => Console.WriteLine("Search started!");
-                systemVisitor.FinishEvent += () => Console.WriteLine("Search finished!");
-
-                systemVisitor.FoundItem += (x) =>
+                FilterSetting filterSetting = new((path, filterPredicate) =>
                 {
-                    var itemType = x.IsFolder ? "Directory" : "File";
-                    Console.WriteLine($"{itemType}  found: {x.Name}");
-                };
+                    FileSystemVisitor systemVisitor = new(filterPredicate);
 
-                systemVisitor.FoundFilteredItem += (x) =>
-                {
-                    var itemType = x.IsFolder ? "directory" : "file";
-                    Console.WriteLine($"Filtered {itemType} found: {x.Name}");
-                    Console.WriteLine("Exclude item(1), Keep item(2), Abort search(3)?");
-                    var userInput = Convert.ToInt32(Console.ReadLine());
-                    if (userInput == (int)ActionType.AbortSearch)
-                        infinitySearch = false;
-                    return (ActionType)userInput;
-                };
+                    systemVisitor.StartEvent += () => PrintToConsole("Search started!");
+                    systemVisitor.FinishEvent += () => PrintToConsole("Search finished!");
 
-                var array = systemVisitor.Search(@"C:\FileSystem");
+                    systemVisitor.FoundItem += (x) =>
+                    {
+                        var itemType = x.IsFolder ? "Directory" : "File";
+                        PrintToConsole($"{itemType}  found: {x.Name}");
+                    };
 
-                if (infinitySearch) PrintToConsole(array);
+                    systemVisitor.FoundFilteredItem += (x) =>
+                    {
+                        var itemType = x.IsFolder ? "directory" : "file";
 
-            } while (infinitySearch);
-            */
+                        PrintToConsole($"Filtered {itemType} found: {x.Name}");
+                        PrintToConsole("Exclude item(1), Keep item(2), Abort search(3)?");
+
+                        var userInput = Convert.ToInt32(Console.ReadLine());
+                        if (userInput == (int)ActionType.AbortSearch)
+                            infinity = false;
+
+                        return (ActionType)userInput;
+                    };
+
+                    var array = systemVisitor.Search(path);
+
+                    PrintToConsole(array);
+                });
+
+                filterSetting.PassConsoleInput(args);
+
+            } while (infinity);
         }
+
         public static void PrintToConsole(IEnumerable<SearchedItem> array)
         {
             Console.WriteLine("Search result:");
@@ -67,5 +54,7 @@ namespace FileSystemSearch
                 Console.WriteLine(item);
             }
         }
+
+        public static void PrintToConsole(string message) => Console.WriteLine(message);
     }
 }
