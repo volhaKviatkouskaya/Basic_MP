cd0ae9bc0e2b037b7fe2feaa57b7fb37e94ebcf7
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-11-03 22:06:36 +0100

    refactoring

--- Module_05/Task2/NumberParser.cs
+++ Module_05/Task2/NumberParser.cs
@@ -5,23 +5,34 @@ namespace Task2
     public class NumberParser : INumberParser
     {
         private const int Diff = 48;
+
         public int Parse(string stringValue)
         {
-            if (stringValue == null) throw new ArgumentNullException();
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException();
+            }
 
             var length = stringValue.Length;
 
-            if (length == 0) throw new FormatException();
+            if (length == 0)
+            {
+                throw new FormatException();
+            }
 
             int result = 0;
 
             for (int i = 0; i < length; i++)
             {
                 if ((stringValue[i] == '-' || stringValue[i] == '+') && i == 0)
+                {
                     continue;
+                }
 
                 if (i != 0 && char.IsWhiteSpace(stringValue[i]))
+                {
                     continue;
+                }
 
                 if (char.IsNumber(stringValue[i]))
                 {
@@ -40,7 +51,9 @@ namespace Task2
                     }
                 }
                 else
+                {
                     throw new FormatException();
+                }
             }
 
             return result;
