0c7ed6070fed97227f9f97c0c38bf28052b1ee55
Volha <77961930+volhaKuraleniaE@users.noreply.github.com>
2022-11-16 10:09:23 +0100

    Module 06 task 01 (#14)
    * start point
    
    * development
    
    * 1
    
    * 2
    
    * CustomItemManager, ConfigurationItem Attribute
    
    * implement types
    
    * added Json file and FileProvider
    
    * added interface and provider classes
    
    * update/save file
    
    * update/save file
    
    * refactoring
    
    Co-authored-by: Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>

---
+++ Module_06/Task_01/CustomAttribute/ConfigurationProvider.cs
@@ -0,0 +1,65 @@
+﻿using System.Configuration;
+
+namespace CustomAttribute
+{
+    public class ConfigurationProvider : IProvider
+    {
+        private readonly Dictionary<string, string> _configurationProvider;
+
+        public ConfigurationProvider()
+        {
+            _configurationProvider = GetAppSettings();
+        }
+
+        private Dictionary<string, string> GetAppSettings()
+        {
+            Dictionary<string, string> appSettingsData = new();
+
+            foreach (string key in ConfigurationManager.AppSettings)
+            {
+                if (key != null)
+                {
+                    var value = ConfigurationManager.AppSettings[key];
+
+                    if (value != null)
+                    {
+                        appSettingsData.Add(key, value);
+                    }
+                }
+            }
+
+            return appSettingsData;
+        }
+
+        public string GetValue(string key)
+        {
+            return _configurationProvider[key];
+        }
+
+        public void SetValue(string key, string value)
+        {
+            _configurationProvider[key] = value;
+        }
+
+        public void SaveChanges()
+        {
+            Configuration configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            KeyValueConfigurationCollection settings = configFile.AppSettings.Settings;
+
+            foreach (var pair in _configurationProvider)
+            {
+                if (settings[pair.Key] == null)
+                {
+                    settings.Add(pair.Key, pair.Value);
+                }
+                else
+                {
+                    settings.Remove(pair.Key);
+                    settings.Add(pair.Key, pair.Value);
+                }
+            }
+
+            configFile.Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
---
+++ Module_06/Task_01/CustomAttribute/CustomItemManager.cs
@@ -0,0 +1,134 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Reflection.Metadata;
+
+namespace CustomAttribute
+{
+    public class CustomItemManager
+    {
+        private const string ConfigurationProvider = "ConfigurationProvider";
+        private const string FileProvider = "FileProvider";
+
+        private readonly Dictionary<string, IProvider> _providers;
+
+        public CustomItemManager()
+        {
+            _providers = new Dictionary<string, IProvider>
+                        {{ConfigurationProvider, new ConfigurationProvider() },
+                        { FileProvider, new FileProvider() } };
+        }
+
+        public void ReadFromFile(CustomItem item)
+        {
+            var customTypePropAttributes = ReturnItemProperties(typeof(CustomItem));
+
+            AssignItemProperties(item, customTypePropAttributes);
+        }
+
+        public void WriteToFile(CustomItem item)
+        {
+            var customTypePropAttributes = ReturnItemProperties(typeof(CustomItem));
+
+            WriteItemProperties(item, customTypePropAttributes);
+
+        }
+
+        private void WriteItemProperties(CustomItem item, Dictionary<string, ConfigurationItemAttribute> dataOfType)
+        {
+            foreach (var keyValuePair in dataOfType)
+            {
+                var propertyInfo = item.GetType().GetProperty(keyValuePair.Key);
+                var attributes = propertyInfo.GetCustomAttributes();
+
+                foreach (var attribute in attributes)
+                {
+                    if (attribute is ConfigurationItemAttribute)
+                    {
+                        var key = keyValuePair.Value.SettingName;
+                        var value = propertyInfo.GetValue(item).ToString();
+                        var provider = keyValuePair.Value.ProviderType;
+
+                        SetPropertyValue(key, value, provider);
+                    }
+                }
+            }
+
+            var providers = dataOfType.Values.Select(x => x.ProviderType).Distinct();
+            foreach (var provider in providers)
+            {
+                SaveChanges(provider);
+            }
+        }
+
+        private Dictionary<string, ConfigurationItemAttribute> ReturnItemProperties(Type type)
+        {
+            var propertyInfo = type.GetProperties();
+
+            Dictionary<string, ConfigurationItemAttribute> pairs = new();
+
+            foreach (var property in propertyInfo)
+            {
+                var customAttributes = property.GetCustomAttributes();
+
+                foreach (var attribute in customAttributes)
+                {
+                    if (attribute is ConfigurationItemAttribute)
+                    {
+                        pairs.Add(property.Name, (ConfigurationItemAttribute)attribute);
+                    }
+                }
+            }
+
+            return pairs;
+        }
+
+        private void AssignItemProperties(object obj, Dictionary<string, ConfigurationItemAttribute> dataOfType)
+        {
+            foreach (var pair in dataOfType)
+            {
+                var pairSettingName = pair.Value.SettingName;
+                var pairValue = GetPropertyValue(pairSettingName, pair.Value.ProviderType);
+                var propertyType = obj.GetType().GetProperty(pair.Key).PropertyType;
+
+                var adjustedValue = Convert(pairValue, propertyType);
+
+                if (adjustedValue != null)
+                {
+                    obj.GetType().GetProperty(pair.Key).SetValue(obj, adjustedValue);
+                }
+            }
+        }
+
+        private object? Convert(string pairValue, Type propertyType)
+        {
+            switch (propertyType.Name)
+            {
+                case "String":
+                    return pairValue;
+                case "Int32":
+                    return int.Parse(pairValue);
+                case "Single":
+                    return float.Parse(pairValue);
+                case "TimeSpan":
+                    return TimeSpan.Parse(pairValue);
+                default:
+                    return null;
+            }
+        }
+
+        private void SaveChanges(string provider)
+        {
+            _providers[provider].SaveChanges();
+        }
+
+        private void SetPropertyValue(string key, string value, string provider)
+        {
+            _providers[provider].SetValue(key, value);
+        }
+
+        private string GetPropertyValue(string pairSettingName, string provider)
+        {
+            return _providers[provider].GetValue(pairSettingName);
+        }
+    }
+}
