7ffa85cc96c418323378cac79cffdbc941aa2689
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-11-27 18:44:01 +0100

    add start view of Sum method

---
+++ Module_08/Task_1/StringSumKata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringSumKata
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public string Sum(string num1, string num2)
+        {
+            var firstNum = int.Parse(num1);
+            var secondNum = int.Parse(num2);
+
+            return (firstNum + secondNum).ToString();
+        }
+    }
+}
