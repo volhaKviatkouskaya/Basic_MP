a7ab35ee083ba6e3259380d728300cfeee04c941
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-11-28 16:28:36 +0100

    Add Book and Cart classes

---
+++ Module_08/Task_2/HarryPotterKata/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotterKata
+{
+    public class Book
+    {
+        public string Title;
+        public decimal Price;
+
+        public Book(string title, decimal price)
+        {
+            Title = title;
+            Price = price;
+        }
+    }
+}
---
+++ Module_08/Task_2/HarryPotterKata/UserCart.cs
@@ -0,0 +1,29 @@
+﻿namespace HarryPotterKata
+{
+    public class UserCart
+    {
+        public List<Book> Cart;
+
+        public UserCart()
+        {
+            Cart = new List<Book>();
+        }
+
+        public void AddToCart(Book book)
+        {
+            Cart.Add(book);
+        }
+
+        public decimal CalculateDiscount()
+        {
+            decimal resultPrice = default;
+
+            foreach (Book book in Cart)
+            {
+                resultPrice += book.Price;
+            }
+
+            return resultPrice;
+        }
+    }
+}
