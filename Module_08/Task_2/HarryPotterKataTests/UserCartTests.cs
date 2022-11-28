1c170bbdfc04d53b77defbe559b7a66196e37458
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-11-28 22:45:27 +0100

    Add tests for CalculateDiscount method

--- Module_08/Task_2/HarryPotterKataTests/UserCartTests.cs
+++ Module_08/Task_2/HarryPotterKataTests/UserCartTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HarryPotterKata.Tests
 {
@@ -12,13 +13,13 @@ namespace HarryPotterKata.Tests
         {
             _testBooks = new List<Book>()
             {
-                new("Harry Potter and the Sorcerer’s Stone", 1),
-                new("Harry Potter and the Chamber of Secrets", 2),
-                new(" Harry Potter and the Prisoner of Azkaban", 3),
-                new("Harry Potter and the Goblet of Fire", 4),
-                new("Harry Potter and the Order of the Phoenix", 5),
-                new("Harry Potter and the Half-Blood Prince", 6),
-                new("Harry Potter and the Deathly Hallows", 7),
+                new("Harry Potter and the Sorcerer’s Stone", 8),
+                new("Harry Potter and the Chamber of Secrets", 8),
+                new(" Harry Potter and the Prisoner of Azkaban", 8),
+                new("Harry Potter and the Goblet of Fire", 8),
+                new("Harry Potter and the Order of the Phoenix", 8),
+                new("Harry Potter and the Half-Blood Prince", 8),
+                new("Harry Potter and the Deathly Hallows", 8),
                 new("Harry Potter and the Cursed Child", 8)
             };
         }
@@ -28,24 +29,116 @@ namespace HarryPotterKata.Tests
         [DataRow(8, 8)]
         public void Check_Number_Of_Books(int insertion, int expected)
         {
-            //Arrange
             var userCart = new UserCart();
             var count = 0;
 
-            //Act
             foreach (var book in _testBooks)
             {
-                if (count < insertion)
+                count++;
+                if (count <= insertion)
                 {
                     userCart.AddToCart(book);
                 }
-
-                count++;
             }
             var actual = userCart.Cart.Count;
 
-            //Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(null, 0)]
+        public void Should_Throw_ArgumentNullException(Book itemBook, int expected)
+        {
+            var userCart = new UserCart();
+
+            Assert.ThrowsException<ArgumentNullException>(() => userCart.AddToCart(itemBook));
+            Assert.AreEqual(expected, userCart.Cart.Count);
+        }
+
+        [TestMethod]
+        [DataRow(1, 8)]
+        [DataRow(2, 16)]
+        [DataRow(3, 24)]
+        public void User_Have_No_Discount(int insertion, double expected)
+        {
+            var userCart = new UserCart();
+            var testBook = _testBooks[insertion];
+            var count = 0;
+
+            do
+            {
+                userCart.AddToCart(testBook);
+                count++;
+            } while (count != insertion);
+
+            var actual = userCart.CalculateDiscount();
+
+            Assert.AreEqual((decimal)expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1, 1, 2, 22.8)]
+        [DataRow(1, 2, 2, 22.8)]
+        public void User_Have_Five_Percent_Discount(int bookIndex1, int bookIndex2, int bookIndex3, double expected)
+        {
+            var userCart = new UserCart();
+            userCart.AddToCart(_testBooks[bookIndex1]);
+            userCart.AddToCart(_testBooks[bookIndex2]);
+            userCart.AddToCart(_testBooks[bookIndex3]);
+
+            var actual = userCart.CalculateDiscount();
+
+            Assert.AreEqual((decimal)expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3, 3, 30.4)]
+        public void User_Have_Ten_Percent_Discount(int bookIndex1, int bookIndex2, int bookIndex3, int bookIndex4, double expected)
+        {
+            var userCart = new UserCart();
+
+            userCart.AddToCart(_testBooks[bookIndex1]);
+            userCart.AddToCart(_testBooks[bookIndex2]);
+            userCart.AddToCart(_testBooks[bookIndex3]);
+            userCart.AddToCart(_testBooks[bookIndex4]);
+
+            var actual = userCart.CalculateDiscount();
+
+            Assert.AreEqual((decimal)expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3, 4, 25.6)]
+        public void User_Have_Twenty_Percent_Discount(int bookIndex1, int bookIndex2, int bookIndex3, int bookIndex4, double expected)
+        {
+            var userCart = new UserCart();
+
+            userCart.AddToCart(_testBooks[bookIndex1]);
+            userCart.AddToCart(_testBooks[bookIndex2]);
+            userCart.AddToCart(_testBooks[bookIndex3]);
+            userCart.AddToCart(_testBooks[bookIndex4]);
+
+            var actual = userCart.CalculateDiscount();
+
+            Assert.AreEqual((decimal)expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3, 4, 5, 30.0)]
+        public void User_Have_Twenty_Five_Percent_Discount(int bookIndex1, int bookIndex2, int bookIndex3, int bookIndex4,
+                                                            int bookIndex5, double expected)
+        {
+            var userCart = new UserCart();
+
+            userCart.AddToCart(_testBooks[bookIndex1]);
+            userCart.AddToCart(_testBooks[bookIndex2]);
+            userCart.AddToCart(_testBooks[bookIndex3]);
+            userCart.AddToCart(_testBooks[bookIndex4]);
+            userCart.AddToCart(_testBooks[bookIndex5]);
+
+            var actual = userCart.CalculateDiscount();
+
+            Assert.AreEqual((decimal)expected, actual);
         }
     }
 }
