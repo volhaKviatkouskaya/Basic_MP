b762aa80674778ca0e4cd3a996fe04f009411e33
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-11-29 21:42:41 +0100

    Add first test

---
+++ Module_08/Task_3/CalcStatsKataTests/CalcStatsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcStatsKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcStatsKata.Tests
+{
+    [TestClass]
+    public class CalcStatsTests
+    {
+        [TestMethod]
+        [DataRow(-2, 6, 9, 15, -2, 92, 11)]
+        public void Get_Min_Number(int expected, params int[] numArray)
+        {
+            CalcStats calc = new(numArray);
+
+            var actual = calc.GetMinNumber();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
