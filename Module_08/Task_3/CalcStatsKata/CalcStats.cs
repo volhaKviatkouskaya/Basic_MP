4de10d308d3526e06a04470c64899565b6134013
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-11-29 21:34:55 +0100

    Add class as start point for development

---
+++ Module_08/Task_3/CalcStatsKata/CalcStats.cs
@@ -0,0 +1,67 @@
+﻿namespace CalcStatsKata
+{
+    public class CalcStats
+    {
+        private readonly int[] _sequence;
+
+        public CalcStats(params int[] number)
+        {
+            _sequence = number;
+        }
+
+        public int GetMinNumber()
+        {
+            var minNumber = _sequence[0];
+
+            foreach (var number in _sequence)
+            {
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+
+            return minNumber;
+        }
+
+        public int GetMaxNumber()
+        {
+            var maxNumber = _sequence[0];
+
+            foreach (var number in _sequence)
+            {
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            return maxNumber;
+        }
+
+        public int GetSequenceCount()
+        {
+            var count = 0;
+
+            foreach (var number in _sequence)
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        public decimal GetSequenceAverage()
+        {
+            var count = this.GetSequenceCount();
+            decimal sum = 0;
+
+            foreach (var number in _sequence)
+            {
+                sum += number;
+            }
+
+            return sum / count;
+        }
+    }
+}
