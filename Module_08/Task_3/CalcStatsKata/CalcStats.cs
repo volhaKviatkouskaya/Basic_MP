ef885aeea0ae7647f6920be63e24f43cf84899be
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-11-29 22:18:22 +0100

    Refactored class constructor

--- Module_08/Task_3/CalcStatsKata/CalcStats.cs
+++ Module_08/Task_3/CalcStatsKata/CalcStats.cs
@@ -6,12 +6,7 @@
 
         public CalcStats(params int[] number)
         {
-
-            if (number != null)
-            {
-                _sequence = number;
-            }
-
+            _sequence = number;
         }
 
         public int GetMinNumber()
