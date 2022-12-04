ec0614d9879c0e81be748df3959c54b65cc907d0
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-12-04 20:29:25 +0100

    Replace data type with NVARCHAR and add condition

--- Module_09/SqlDbProject_VS_2022/SqlDb/DeploymentScripts/PostDeploymentScripts/CreateStoredProcedureScripts/Insert_Employee_Info.sql
+++ Module_09/SqlDbProject_VS_2022/SqlDb/DeploymentScripts/PostDeploymentScripts/CreateStoredProcedureScripts/Insert_Employee_Info.sql
@@ -1,16 +1,16 @@
-﻿CREATE PROCEDURE p_insert_employee_info (@EmployeeName VARCHAR(100),
-									@FirstName VARCHAR(50),
-									@LastName VARCHAR(50),
-									@CompanyName VARCHAR(20),
-									@Position VARCHAR(30),
-									@Street VARCHAR(50),
-									@City VARCHAR(20),
-									@State VARCHAR(50),
-									@ZipCode VARCHAR(50))
+﻿CREATE PROCEDURE p_insert_employee_info (@EmployeeName NVARCHAR(100),
+									@FirstName NVARCHAR(50),
+									@LastName NVARCHAR(50),
+									@CompanyName NVARCHAR(50),
+									@Position NVARCHAR(30),
+									@Street NVARCHAR(50),
+									@City NVARCHAR(20),
+									@State NVARCHAR(50),
+									@ZipCode NVARCHAR(50))
 
 AS
 	BEGIN
-		DECLARE @letter_expression varchar(10) SET @letter_expression = '[A-Za-z]%'
+		DECLARE @letter_expression NVARCHAR(10) SET @letter_expression = '[A-Za-z]%'
 	
 		SET @EmployeeName = (CASE WHEN (@EmployeeName IS NULL OR TRIM(@EmployeeName) LIKE '') THEN ' ' 
 									ELSE @EmployeeName END)
@@ -20,6 +20,9 @@ AS
 
 		SET @LastName = (CASE WHEN (@LastName IS NULL OR TRIM(@LastName) LIKE '') THEN ' '
 								ELSE @LastName END)
+
+		SET @CompanyName = (CASE WHEN LEN(@CompanyName)>20 THEN SUBSTRING(@CompanyName,0,20) 
+									ELSE @CompanyName END)
 
 	IF(@EmployeeName LIKE @letter_expression OR @FirstName LIKE @letter_expression OR @LastName LIKE @letter_expression)
 		BEGIN
