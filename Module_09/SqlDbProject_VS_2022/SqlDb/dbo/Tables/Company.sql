3620752da0be854eddc5bfa6e63a5e24a1dc5f0c
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-12-05 16:48:29 +0100

    Add AUTOINCREMENT to id table fields

--- Module_09/SqlDbProject_VS_2022/SqlDb/dbo/Tables/Company.sql
+++ Module_09/SqlDbProject_VS_2022/SqlDb/dbo/Tables/Company.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[company] (
-    [company_id]        INT           NOT NULL,
-    [name]      NVARCHAR (20) NOT NULL,
-    [address_id] INT           NOT NULL,
+    [company_id] INT NOT NULL IDENTITY,
+    [name] NVARCHAR (20) NOT NULL,
+    [address_id] INT NOT NULL,
     CONSTRAINT [PK_company_id] PRIMARY KEY CLUSTERED ([company_id] ASC),
     CONSTRAINT [FK_address_id] FOREIGN KEY ([address_id]) REFERENCES [dbo].[address] ([address_id])
 );
