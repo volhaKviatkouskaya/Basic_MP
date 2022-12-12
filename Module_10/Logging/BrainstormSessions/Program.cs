20394a6a2027ec11b2b080bc163971218cf4d749
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-12-12 21:12:32 +0100

    Add NuGet packages, appSettings file, update Program and Startup

--- Module_10/Logging/BrainstormSessions/Program.cs
+++ Module_10/Logging/BrainstormSessions/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Core;
 
 namespace BrainstormSessions
 {
@@ -7,11 +9,14 @@ namespace BrainstormSessions
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration().CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((hostingContext, loggerConfiguration)=>
+                    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
