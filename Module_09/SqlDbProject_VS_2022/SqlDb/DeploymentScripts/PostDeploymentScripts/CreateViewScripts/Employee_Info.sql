9d56cfa1f791476f98f947e2d751f965b3e932c0
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-12-16 18:59:18 +0100

    Remove sub query from view

--- Module_09/SqlDbProject_VS_2022/SqlDb/DeploymentScripts/PostDeploymentScripts/CreateViewScripts/Employee_Info.sql
+++ Module_09/SqlDbProject_VS_2022/SqlDb/DeploymentScripts/PostDeploymentScripts/CreateViewScripts/Employee_Info.sql
@@ -1,13 +1,9 @@
-﻿CREATE VIEW v_employee_info AS
-  SELECT [employee].[employee_id] AS EmployeeId,
-		 (CASE 
-				WHEN [employee].[employee_name] = null THEN (SELECT [person].[first_name] + ' ' + [person].[last_name]
-														FROM [person])
-				ELSE [employee].[employee_name]
-				END) AS EmployeeFullName,
+﻿CREATE VIEW vv_employee_info AS
+		SELECT [employee].[employee_id] AS EmployeeId, 
+				[employee].[employee_name] AS EmployeeFullName,
 				CONCAT([address].[zip_code], '_', [address].[state], ',', [address].[city], '-', [address].[street]) AS EmployeeFullAddress,
 				CONCAT([employee].[company_name], '(', [employee].[position], ')') AS EmployeeCompanyInfo
-  FROM [employee]
-  JOIN [address] ON [employee].[address_id] = [address].[address_id]
-  JOIN [person] ON [employee].[person_id] = [person].[person_id]
-  ORDER BY [employee].[company_name], [address].[city] ASC offset 0 ROWS
+		FROM [employee]
+		JOIN [address] ON [employee].[address_id] = [address].[address_id]
+		JOIN [person] ON [employee].[person_id] = [person].[person_id]
+		ORDER BY [employee].[company_name], [address].[city] ASC offset 0 ROWS
