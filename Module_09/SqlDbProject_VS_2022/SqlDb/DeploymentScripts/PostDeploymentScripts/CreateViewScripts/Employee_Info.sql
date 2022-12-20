a797dcd32a66156dfc9830809022b8c872ee4e60
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-12-20 19:31:05 +0100

    Update view creation script

--- Module_09/SqlDbProject_VS_2022/SqlDb/DeploymentScripts/PostDeploymentScripts/CreateViewScripts/Employee_Info.sql
+++ Module_09/SqlDbProject_VS_2022/SqlDb/DeploymentScripts/PostDeploymentScripts/CreateViewScripts/Employee_Info.sql
@@ -1,14 +1,9 @@
 ﻿CREATE VIEW v_employee_info AS
-		SELECT [employee].[employee_id] AS EmployeeId, 
-				EmployeeFullName = CASE 
-										WHEN [employee].[employee_name] = NULL THEN (SELECT CONCAT(first_name, ' ', last_name)
-																					FROM [dbo].[person]
-																					JOIN [dbo].[employee] ON [dbo].[person].person_id = [dbo].[employee].person_id)
-										ELSE [employee].[employee_name]
-									END,
-				CONCAT([address].[zip_code], '_', [address].[state], ',', [address].[city], '-', [address].[street]) AS EmployeeFullAddress,
+		SELECT [employee].[employee_id] AS EmployeeId,
+				CONCAT(p.first_name, p.last_name) AS EmployeeFullName,
+				CONCAT(a.[zip_code], '_', a.[state], ',', a.[city], '-', a.[street]) AS EmployeeFullAddress,
 				CONCAT([employee].[company_name], '(', [employee].[position], ')') AS EmployeeCompanyInfo
 		FROM [employee]
-		JOIN [address] ON [employee].[address_id] = [address].[address_id]
-		JOIN [person] ON [employee].[person_id] = [person].[person_id]
-		ORDER BY [employee].[company_name], [address].[city] ASC offset 0 ROWS
+		JOIN [address] AS a ON [employee].[address_id] = a.[address_id]
+		JOIN [person] AS p ON [employee].[person_id] = p.[person_id]
+		ORDER BY [employee].[company_name], a.[city] ASC offset 0 ROWS
