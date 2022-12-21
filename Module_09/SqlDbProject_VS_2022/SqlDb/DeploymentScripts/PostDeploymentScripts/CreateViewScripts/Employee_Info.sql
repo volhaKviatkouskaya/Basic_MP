bb1254d4b127849cdff19041de15f413ba0c80b4
Volha <77961930+volhaKviatkouskaya@users.noreply.github.com>
2022-12-21 19:51:54 +0100

    Module 09 development (#22)
    * Add empty solution
    
    * Add DB tables and Script.PostDeployment
    
    * Create EmployeeInfo view
    
    * Add InsertEmployeeInfo stored procedure
    
    * Change folders structure and follow naming convention
    
    * Add condition to procedure
    
    * Replace data type with NVARCHAR and add condition
    
    * Add trigger to Employee table
    
    * Add AUTOINCREMENT to id table fields
    
    * Add AUTOINCREMENT to id table fields
    
    * Replace + with concat method for task_2
    
    * Remove sub query from view
    
    * Alter trigger and update procedure with scope_identity
    
    * Add condition to view
    
    * Update view creation script
    
    * Return null checking for employee_name

---
+++ Module_09/SqlDbProject_VS_2022/SqlDb/DeploymentScripts/PostDeploymentScripts/CreateViewScripts/Employee_Info.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW v_employee_info AS
+		SELECT [employee].[employee_id] AS EmployeeId,
+				(CASE WHEN [employee].[employee_name] = NULL THEN CONCAT(p.first_name, ' ', p.last_name)
+						ELSE [employee].[employee_name]
+						END) AS EmployeeFullName,
+				CONCAT(a.[zip_code], '_', a.[state], ',', a.[city], '-', a.[street]) AS EmployeeFullAddress,
+				CONCAT([employee].[company_name], '(', [employee].[position], ')') AS EmployeeCompanyInfo
+		FROM [employee]
+		JOIN [address] AS a ON [employee].[address_id] = a.[address_id]
+		JOIN [person] AS p ON [employee].[person_id] = p.[person_id]
+		ORDER BY [employee].[company_name], a.[city] ASC offset 0 ROWS
