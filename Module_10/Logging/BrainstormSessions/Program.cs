30901b25e5db53811994c6d626bc5a2de052050a
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-12-25 19:41:53 +0100

    Replace Log4net with Serilog for BrainstormSessions project, add mail sender

--- Module_10/Logging/BrainstormSessions/Program.cs
+++ Module_10/Logging/BrainstormSessions/Program.cs
@@ -1,8 +1,12 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System.IO;
+using System.Net;
 using log4net.Config;
+using Serilog.Events;
+using Serilog.Sinks.Email;
 
 namespace BrainstormSessions
 {
@@ -10,14 +14,27 @@ namespace BrainstormSessions
     {
         public static void Main(string[] args)
         {
-            FileInfo configFile = new FileInfo("web.config");
-            XmlConfigurator.Configure(configFile);
+            var senderEmail = Environment.GetEnvironmentVariable("SENDER");
+            var password = Environment.GetEnvironmentVariable("PASSWORD");
+            var receiverEmail = Environment.GetEnvironmentVariable("RECEIVER");
 
             Log.Logger = new LoggerConfiguration()
                 .Enrich.WithProperty("Logging", "Program")
                 .MinimumLevel.Information()
-                .WriteTo.Log4Net()
-                .WriteTo.File("Logs/structuredLog.log")
+                .WriteTo.File("Logs/structuredLog.json")
+                .WriteTo.Email(restrictedToMinimumLevel: LogEventLevel.Information,
+                    period: TimeSpan.FromSeconds(5), 
+                    batchPostingLimit:2,
+                    connectionInfo: new EmailConnectionInfo
+                    {
+                        EmailSubject = "APP Logs",
+                        NetworkCredentials = new NetworkCredential(senderEmail, password),
+                        FromEmail = senderEmail,
+                        ToEmail = receiverEmail,
+                        MailServer = "smtp.gmail.com",
+                        EnableSsl = true,
+                        Port = 465
+                    })
                 .CreateLogger();
 
             CreateHostBuilder(args).Build().Run();
@@ -28,6 +45,7 @@ namespace BrainstormSessions
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
